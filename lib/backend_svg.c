86a84fc0ad1d2f8c86c31925524588841672c80d
Frank Chung <frank_chung@htc.com>
2019-12-10 01:40:51 +0800

    add potrace implementation

---
+++ lib/backend_svg.c
@@ -0,0 +1,315 @@
+/* Copyright (C) 2001-2019 Peter Selinger.
+   This file is part of Potrace. It is free software and it is covered
+   by the GNU General Public License. See the file COPYING for details. */
+
+/* The SVG backend of Potrace. */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+
+#include "potracelib.h"
+#include "curve.h"
+#include "potrace.h"
+#include "backend_svg.h"
+#include "lists.h"
+#include "auxiliary.h"
+
+#define UNIT 10
+
+/* ---------------------------------------------------------------------- */
+/* path-drawing auxiliary functions */
+
+/* coordinate quantization */
+static inline point_t unit(dpoint_t p)
+{
+  point_t q;
+
+  q.x = (long)(floor(p.x * UNIT + .5));
+  q.y = (long)(floor(p.y * UNIT + .5));
+  return q;
+}
+
+static point_t cur;
+static char lastop = 0;
+static int column = 0;
+static int newline = 1;
+
+static void shiptoken(FILE *fout, const char *token)
+{
+  int c = strlen(token);
+  if (!newline && column + c + 1 > 75)
+  {
+    fprintf(fout, " ");
+    column = 0;
+    newline = 1;
+  }
+  else if (!newline)
+  {
+    fprintf(fout, " ");
+    column++;
+  }
+  fprintf(fout, "%s", token);
+  column += c;
+  newline = 0;
+}
+
+static void ship(FILE *fout, const char *fmt, ...)
+{
+  va_list args;
+  static char buf[4096]; /* static string limit is okay here because
+			    we only use constant format strings - for
+			    the same reason, it is okay to use
+			    vsprintf instead of vsnprintf below. */
+  char *p, *q;
+
+  va_start(args, fmt);
+  vsprintf(buf, fmt, args);
+  buf[4095] = 0;
+  va_end(args);
+
+  p = buf;
+  while ((q = strchr(p, ' ')) != NULL)
+  {
+    *q = 0;
+    shiptoken(fout, p);
+    p = q + 1;
+  }
+  shiptoken(fout, p);
+}
+
+static void svg_moveto(FILE *fout, dpoint_t p)
+{
+  cur = unit(p);
+
+  ship(fout, "M%ld %ld", cur.x, cur.y);
+  lastop = 'M';
+}
+
+static void svg_rmoveto(FILE *fout, dpoint_t p)
+{
+  point_t q;
+
+  q = unit(p);
+  ship(fout, "m%ld %ld", q.x - cur.x, q.y - cur.y);
+  cur = q;
+  lastop = 'm';
+}
+
+static void svg_lineto(FILE *fout, dpoint_t p)
+{
+  point_t q;
+
+  q = unit(p);
+
+  if (lastop != 'l')
+  {
+    ship(fout, "l%ld %ld", q.x - cur.x, q.y - cur.y);
+  }
+  else
+  {
+    ship(fout, "%ld %ld", q.x - cur.x, q.y - cur.y);
+  }
+  cur = q;
+  lastop = 'l';
+}
+
+static void svg_curveto(FILE *fout, dpoint_t p1, dpoint_t p2, dpoint_t p3)
+{
+  point_t q1, q2, q3;
+
+  q1 = unit(p1);
+  q2 = unit(p2);
+  q3 = unit(p3);
+
+  if (lastop != 'c')
+  {
+    ship(fout, "c%ld %ld %ld %ld %ld %ld", q1.x - cur.x, q1.y - cur.y, q2.x - cur.x, q2.y - cur.y, q3.x - cur.x, q3.y - cur.y);
+  }
+  else
+  {
+    ship(fout, "%ld %ld %ld %ld %ld %ld", q1.x - cur.x, q1.y - cur.y, q2.x - cur.x, q2.y - cur.y, q3.x - cur.x, q3.y - cur.y);
+  }
+  cur = q3;
+  lastop = 'c';
+}
+
+/* ---------------------------------------------------------------------- */
+/* functions for converting a path to an SVG path element */
+
+/* Explicit encoding. If abs is set, move to first coordinate
+   absolutely. */
+static int svg_path(FILE *fout, potrace_curve_t *curve, int abs)
+{
+  int i;
+  dpoint_t *c;
+  int m = curve->n;
+
+  c = curve->c[m - 1];
+  if (abs)
+  {
+    svg_moveto(fout, c[2]);
+  }
+  else
+  {
+    svg_rmoveto(fout, c[2]);
+  }
+
+  for (i = 0; i < m; i++)
+  {
+    c = curve->c[i];
+    switch (curve->tag[i])
+    {
+    case POTRACE_CORNER:
+      svg_lineto(fout, c[1]);
+      svg_lineto(fout, c[2]);
+      break;
+    case POTRACE_CURVETO:
+      svg_curveto(fout, c[0], c[1], c[2]);
+      break;
+    }
+  }
+  newline = 1;
+  shiptoken(fout, "z");
+  return 0;
+}
+
+/* produce a jaggy path - for debugging. If abs is set, move to first
+   coordinate absolutely. If abs is not set, move to first coordinate
+   relatively, and traverse path in the opposite direction. */
+static int svg_jaggy_path(FILE *fout, point_t *pt, int n, int abs)
+{
+  int i;
+  point_t cur, prev;
+
+  if (abs)
+  {
+    cur = prev = pt[n - 1];
+    svg_moveto(fout, dpoint(cur));
+    for (i = 0; i < n; i++)
+    {
+      if (pt[i].x != cur.x && pt[i].y != cur.y)
+      {
+        cur = prev;
+        svg_lineto(fout, dpoint(cur));
+      }
+      prev = pt[i];
+    }
+    svg_lineto(fout, dpoint(pt[n - 1]));
+  }
+  else
+  {
+    cur = prev = pt[0];
+    svg_rmoveto(fout, dpoint(cur));
+    for (i = n - 1; i >= 0; i--)
+    {
+      if (pt[i].x != cur.x && pt[i].y != cur.y)
+      {
+        cur = prev;
+        svg_lineto(fout, dpoint(cur));
+      }
+      prev = pt[i];
+    }
+    svg_lineto(fout, dpoint(pt[0]));
+  }
+  newline = 1;
+  shiptoken(fout, "z");
+  return 0;
+}
+
+static void write_paths_opaque(FILE *fout, potrace_path_t *tree)
+{
+  potrace_path_t *p, *q;
+
+  for (p = tree; p; p = p->sibling)
+  {
+    column = fprintf(fout, "<path fill=\"#000000\" stroke=\"none\" d=\"");
+    newline = 1;
+    lastop = 0;
+    svg_path(fout, &p->curve, 1);
+    fprintf(fout, "\"/>");
+    for (q = p->childlist; q; q = q->sibling)
+    {
+      column = fprintf(fout, "<path fill=\"#ffffff\" stroke=\"none\" d=\"");
+      newline = 1;
+      lastop = 0;
+      svg_path(fout, &q->curve, 1);
+      fprintf(fout, "\"/>");
+    }
+    for (q = p->childlist; q; q = q->sibling)
+    {
+      write_paths_opaque(fout, q->childlist);
+    }
+  }
+}
+
+static void write_paths_transparent_rec(FILE *fout, potrace_path_t *tree)
+{
+  potrace_path_t *p, *q;
+
+  for (p = tree; p; p = p->sibling)
+  {
+    column = fprintf(fout, "<path d=\"");
+    newline = 1;
+    lastop = 0;
+
+    svg_path(fout, &p->curve, 1);
+
+    for (q = p->childlist; q; q = q->sibling)
+    {
+      svg_path(fout, &q->curve, 0);
+    }
+
+    fprintf(fout, "\"/>");
+
+    for (q = p->childlist; q; q = q->sibling)
+    {
+      write_paths_transparent_rec(fout, q->childlist);
+    }
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+/* Backend. */
+
+/* public interface for SVG */
+int page_svg(FILE *fout, potrace_path_t *plist, imginfo_t *imginfo)
+{
+
+  double bboxx = imginfo->trans.bb[0] + imginfo->lmar + imginfo->rmar;
+  double bboxy = imginfo->trans.bb[1] + imginfo->tmar + imginfo->bmar;
+  double origx = imginfo->trans.orig[0] + imginfo->lmar;
+  double origy = bboxy - imginfo->trans.orig[1] - imginfo->bmar;
+  double scalex = imginfo->trans.scalex / UNIT;
+  double scaley = -imginfo->trans.scaley / UNIT;
+
+  /* header */
+  fprintf(fout, "<?xml version=\"1.0\" standalone=\"no\"?>");
+  fprintf(fout, "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 20010904//EN\"");
+  fprintf(fout, " \"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd\">");
+
+  /* set bounding box and namespace */
+  fprintf(fout, "<svg version=\"1.0\" xmlns=\"http://www.w3.org/2000/svg\"");
+  fprintf(fout, " width=\"%fpt\" height=\"%fpt\" viewBox=\"0 0 %f %f\"",
+          bboxx, bboxy, bboxx, bboxy);
+  fprintf(fout, " preserveAspectRatio=\"xMidYMid meet\">");
+
+  /* use a "group" tag to establish coordinate system and style */
+  fprintf(fout, "<g transform=\"");
+  if (origx != 0 || origy != 0)
+  {
+    fprintf(fout, "translate(%f,%f) ", origx, origy);
+  }
+  fprintf(fout, "scale(%f,%f)\" ", scalex, scaley);
+  fprintf(fout, "fill=\"#000000\" stroke=\"none\">");
+
+  write_paths_transparent_rec(fout, plist);
+
+  /* write footer */
+  fprintf(fout, "</g>");
+  fprintf(fout, "</svg>");
+  fflush(fout);
+
+  return 0;
+}
--- lib/potrace.c
+++ lib/potrace.c
@@ -1,10 +1,117 @@
+/* Copyright (C) 2001-2019 Peter Selinger.
+   This file is part of Potrace. It is free software and it is covered
+   by the GNU General Public License. See the file COPYING for details. */
+
 #include <stdio.h>
 #include <stdint.h>
 
+#include "potrace.h"
+#include "potracelib.h"
+#include "bitmap.h"
+#include "backend_svg.h"
+
+#define UNDEF ((double)(1e30)) /* a value to represent "undefined" */
+
+/* ---------------------------------------------------------------------- */
+/* calculations with bitmap dimensions, positioning etc */
+
+/* determine the dimensions of the output based on command line and
+   image dimensions, and optionally, based on the actual image outline. */
+static void calc_dimensions(imginfo_t *imginfo, potrace_path_t *plist)
+{
+    /* we take care of a special case: if one of the image dimensions is
+     0, we change it to 1. Such an image is empty anyway, so there
+     will be 0 paths in it. Changing the dimensions avoids division by
+     0 error in calculating scaling factors, bounding boxes and
+     such. This doesn't quite do the right thing in all cases, but it
+     is better than causing overflow errors or "nan" output in
+     backends.  Human users don't tend to process images of size 0
+     anyway; they might occur in some pipelines. */
+    if (imginfo->pixwidth == 0)
+    {
+        imginfo->pixwidth = 1;
+    }
+    if (imginfo->pixheight == 0)
+    {
+        imginfo->pixheight = 1;
+    }
+
+    /* apply default dimension to width, height, margins */
+    imginfo->lmar = 0;
+    imginfo->rmar = 0;
+    imginfo->tmar = 0;
+    imginfo->bmar = 0;
+
+    /* start with a standard rectangle */
+    trans_from_rect(&imginfo->trans, imginfo->pixwidth, imginfo->pixheight);
+
+    /* if width and height are still variable, tenatively use the
+     default scaling factor of 72dpi (for dimension-based backends) or
+     1 (for pixel-based backends). For fixed-size backends, this will
+     be adjusted later to fit the page. */
+    imginfo->width = imginfo->trans.bb[0];
+    imginfo->height = imginfo->trans.bb[1];
+
+    /* apply scaling */
+    trans_scale_to_size(&imginfo->trans, imginfo->width, imginfo->height);
+}
+
 const char *convert_svg(uint8_t pixels[], int width, int height)
 {
-    printf("potrace receives %d bytes\n", width * height);
-    return "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"100\" height=\"100\">"
-           "<path d=\"M10 10 L 60 10 L 60 60 L 10 60z\"/>"
-           "</svg>";
+    // initialize the bitmap with given pixels.
+    potrace_bitmap_t *bm = bm_new(width, height);
+    for (int i = 0; i < width * height * 4; i += 4)
+    {
+        int index = i / 4;
+        int x = index % width;
+        int y = height - (index / width) - 1;
+
+        // Relative luminance
+        int color = 0.2126 * pixels[i] + 0.7152 * pixels[i + 1] + 0.0722 * pixels[i + 2];
+        int a = pixels[i + 3];
+        BM_UPUT(bm, x, y, a != 0 && color < 128);
+    }
+
+    // start the potrace algorithm.
+    potrace_param_t param = {
+        .turdsize = 2,
+        .turnpolicy = 4,
+        .alphamax = 1,
+        .opticurve = 1,
+        .opttolerance = 0.2,
+    };
+
+    potrace_state_t *st = potrace_trace(&param, bm);
+    if (!st || st->status != POTRACE_STATUS_OK)
+    {
+        fprintf(stderr, "trace error: %s\n", strerror(errno));
+        exit(2);
+    }
+
+    // conver the trace to image information for svg backend generation.
+    imginfo_t imginfo = {
+        .pixwidth = bm->w,
+        .pixheight = bm->h,
+    };
+    bm_free(bm);
+
+    // calculte the dimension of image.
+    calc_dimensions(&imginfo, st->plist);
+
+    // start convert to svg.
+    char *buf;
+    size_t len;
+    FILE *stream = open_memstream(&buf, &len);
+    int r = page_svg(stream, st->plist, &imginfo);
+    if (r)
+    {
+        fprintf(stderr, "page_svg error: %s\n", strerror(errno));
+        exit(2);
+    }
+
+    // release resource.
+    fclose(stream);
+    potrace_state_free(st);
+
+    return buf;
 }
