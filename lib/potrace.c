5b9c759453904f2f73ae9c95fd682299941140fc
Frank Chung <frank_chung@htc.com>
2019-12-09 17:53:19 +0800

    pass image from browser

--- lib/potrace.c
+++ lib/potrace.c
@@ -4,5 +4,7 @@
 const char *convert_svg(uint8_t pixels[], int width, int height)
 {
     printf("potrace receives %d bytes\n", width * height);
-    return "<svg></svg>";
+    return "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"100\" height=\"100\">"
+           "<path d=\"M10 10 L 60 10 L 60 60 L 10 60z\"/>"
+           "</svg>";
 }
